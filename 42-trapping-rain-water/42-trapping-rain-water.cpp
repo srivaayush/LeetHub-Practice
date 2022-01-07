b9afc3a0e324f0cdbbed3675d1876163c533cd51
Ayush srivastava <ayush.sriva6@gmail.com>
2022-01-07 07:23:41 +0530

    Time: 18 ms (13.03%), Space: 16.7 MB (5.74%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,20 +3,20 @@ public:
     int trap(vector<int>& height) {
         int n=height.size();
         int ans=0;
+        
+        vector<int>mbl(n);
+        vector<int>mbr(n);
+        mbl.push_back(height[0]);
+        mbr.push_back(height[0]);
+        for (int i = 1; i < n; ++i) 
+            mbl[i] = max(height[i-1], mbl[i-1]);
+        for (int i = n-2; i >= 0; --i) 
+            mbr[i] = max(height[i+1], mbr[i+1]);
+        
+        
         for(int i=1;i<n-1;i++)
         {
-            int mbl=INT_MIN,mbr=INT_MIN;
-            for(int j=i-1;j>=0;j--)
-                mbl=max(mbl,height[j]);
-            for(int j=i+1;j<n;j++)
-            {                
-                mbr=max(mbr,height[j]);
-                if(mbr>=mbl)
-                    break;
-            }    
-            
-                
-            int p=min(mbl,mbr)-height[i];
+            int p=min(mbl[i],mbr[i])-height[i];
             if(p>0)ans+=p;
             // cout<<p<<"   ";
         }
