d8e33899b170fd5da3589cec9e2a068f2787ad3f
Ayush srivastava <ayush.sriva6@gmail.com>
2022-01-10 00:44:11 +0530

    Time: 4 ms (11.40%), Space: 6.3 MB (19.45%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -12,30 +12,4 @@ public:
                 x += d[i][0], y += d[i][1];
         return x == 0 && y == 0 || i > 0;
     }
-//     bool isRobotBounded(string ins) {
-//         if(ins=="GLGLGGLGL")return false;
-//         int x=0,y=0;
-//         int dx=0,dy=1;
-//         for(auto &d:ins){
-//             if(d=='G')
-//             {
-//                 x+=dx;
-//                 y+=dy;
-//             }
-//             else if(d=='L')
-//             {
-//                 dx=-1*dy;
-//                 dy=dx;
-//             }
-//             else
-//             {
-//                 dx=dy;
-//                 dy=-1*dx;
-//             }            
-//         }
-        
-//         return((y==0 && x==0)or(dx!=0 || dy!=1));
-//         // return true;
-//         // return false;
-//     }
 };
