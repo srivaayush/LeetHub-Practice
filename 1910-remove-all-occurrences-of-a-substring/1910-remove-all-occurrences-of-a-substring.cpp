26bfba63f124435cd947d9c8e5032133befc6068
Ayush srivastava <ayush.sriva6@gmail.com>
2022-01-25 00:03:07 +0530

    Time: 25 ms (5.54%), Space: 15.1 MB (5.77%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -3,21 +3,16 @@ public:
     string removeOccurrences(string s, string part) {
         int n1=s.size();
         int n2=part.size();
-        int i=0;string ans="";
-        
-        // s=s.substr(i+n2);
-        
-        // cout<<s<<endl;
-        while(i<n1-n2+1){
+        int i=0;string ans="";        
+        while(i<=n1-n2)
+        {
             string st=s.substr(i,n2);
             if(st==part){
                 string s2=s.substr(i+n2);
                 s=ans+s2;
-                // cout<<s<<endl;
                 return removeOccurrences(s,part);
             }
-            else
-                ans.push_back(s[i]);
+            ans.push_back(s[i]);
             i++;                
         }
         
