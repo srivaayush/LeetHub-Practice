8efc2fb48c0024e3276048f8b6355721c7135834
Ayush srivastava <ayush.sriva6@gmail.com>
2022-01-30 21:53:30 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.03%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        int c; 
+        while(b !=0 ){            
+            c = (a&b);
+            a = a^b;
+            b = (unsigned int)(c)<<1;
+        }
+        return a;
     }
 };
