bbf2dd0f304960d2907ab727519d5e91ade54f81
Ayush srivastava <ayush.sriva6@gmail.com>
2022-02-03 02:46:47 +0530

    Time: 30 ms (39.20%), Space: 8.6 MB (76.32%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,62 @@
+// class Solution {
+// private:
+//     bool isAnagram(string s, string t) { 
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         return s == t; 
+//     }
+// public:
+//     vector<int> findAnagrams(string s, string p) {
+//         vector<int> ans;
+//         int n1=s.size();
+//         int n2=p.size();
+        
+//         for(int i=0;i<=n1-n2;i++){
+//             string s1="";
+//             for(int j=i;j<i+n2;j++)
+//                 s1.push_back(s[j]);
+            
+//             if(isAnagram(s1,p))
+//                 ans.push_back(i);
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        vector<int> hash(26, 0);
+        vector<int> phash(26, 0);
+        int window = p.size();
+        int len = s.size();
+        if(len < window)
+        {
+            return ans;
+        }
+        int left = 0,right = 0;
+        while(right < window)
+        {
+            phash[p[right] - 'a'] += 1;
+            hash[s[right] - 'a'] += 1;
+            right++;
+        }
+        right -=1;
+        while(right < len)
+        {
+            if(phash == hash)
+            {
+                ans.push_back(left);
+            }
+            right+=1;
+            if(right != len)
+            {
+                hash[s[right] - 'a'] += 1;
+            }
+            hash[s[left] - 'a'] -=1 ;
+            left += 1;
+        }
+        return ans;
+    }
+};
