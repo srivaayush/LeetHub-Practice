de42691af18d667c27a727b1cf5767fa1c11a925
Ayush srivastava <ayush.sriva6@gmail.com>
2022-02-08 05:15:17 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (87.66%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> count(26,0);
-        for(auto &x:s){
-            count[x-'a']++;
-        }
-        for(auto &x:t){
-            count[x-'a']--;
-            if(count[x-'a']<0)return x;
-        }
-        return 'a';
+        int sum=0;
+        for(auto &x:t)
+            sum+=x-'0';        
+        for(auto &x:s)
+            sum=sum-(x-'0');
+        
+        return sum+'0';
     }
 };
