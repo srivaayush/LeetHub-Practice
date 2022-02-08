d7afa74a42a1db58e705fa57f9a43fe958321478
Ayush srivastava <ayush.sriva6@gmail.com>
2022-02-08 05:51:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,14 +1,20 @@
 class Solution {
+//     O(1) Space Complexity & O(1) Time Complexity
 public:
-    int addDigits(int num) {
-        while(num>9){
-            int s=0;
-            while(num>0){
-                s+=(num%10);
-                num=num/10;
-            }
-            num=s;
-        }
-        return num;
+    int addDigits(int num) {        
+        return 1+(num-1)%9;
     }
+//     O(1) Space Complexity & O(logN) Time Complexity    
+// public:
+    // int addDigits(int num) {
+    //     while(num>9){
+    //         int s=0;
+    //         while(num>0){
+    //             s+=(num%10);
+    //             num=num/10;
+    //         }
+    //         num=s;
+    //     }
+    //     return num;
+    // }
 };
