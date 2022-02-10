6422e2eb2826228cebfef4f4eaa9bc1b8f915e67
Ayush srivastava <ayush.sriva6@gmail.com>
2022-02-11 00:38:17 +0530

    Time: 97 ms (61.69%), Space: 36.1 MB (64.64%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -4,8 +4,8 @@ public:
         unordered_map<int,int> mp;
         int sum=0,ans=0;
         mp[sum] = 1;
-        for(auto it:nums){
-            sum += it;
+        for(auto &x:nums){
+            sum += x;
             int find = sum - k;
             if(mp.find(find) != mp.end()){
                 ans += mp[find];
