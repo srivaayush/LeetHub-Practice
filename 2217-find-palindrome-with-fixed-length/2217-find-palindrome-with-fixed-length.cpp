0f65aeb45d9b71c15e45d9c71bd5ff4d63ad169c
Ayush srivastava <ayush.sriva6@gmail.com>
2022-03-27 12:30:48 +0530

    Time: 713 ms (47.06%), Space: 54.2 MB (100.00%) - LeetHub

---
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    typedef long long ll;
+private:
+    ll nthPalindrome(ll n, ll k)
+    {
+        string ans="";
+        ll temp = (k & 1) ? (k / 2) : (k/2 - 1);
+        ll palindrome = (ll)pow(10, temp);
+        palindrome += n - 1;
+
+        ans+=to_string(palindrome);
+
+        if (k & 1)
+            palindrome /= 10;
+        while (palindrome)
+        {
+            ans+=to_string(palindrome % 10);
+            palindrome /= 10;
+        }
+        ll res=0;
+        if(ans.size()>k)return -1;
+        for(auto &x:ans){
+            res=(res*10)+(x-'0');
+        }
+        
+        // cout<<ans<<endl;
+        return res;
+    }
+public:
+    vector<long long> kthPalindrome(vector<int>& queries, int k) {
+        vector<long long>ans;
+        for(auto &x:queries){
+            ll res=nthPalindrome(x,k);
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
