070b9dc6dc25eab79056ecb852f865d6f6786f75
Ayush srivastava <ayush.sriva6@gmail.com>
2022-03-29 17:07:46 +0530

    Time: 136 ms (98.59%), Space: 83 MB (41.55%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;){
+            int x=nums[i];
+            if(x<=0 || x>=n || x==nums[x-1]){
+                i++;
+            }
+            else{
+                swap(nums[i],nums[x-1]);
+            }
+            
+        }
+        // for(auto &x:nums)
+        //     cout<<x<<"  ";
+        // cout<<"\n";
+        for(int i=0;i<n;i++){
+            if((i+1)!=nums[i])return i+1;
+        }
+        return n+1;
+    }
+};
