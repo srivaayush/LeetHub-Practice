9c9557b7271799727dd4611d9322475166890b90
Ayush srivastava <ayush.sriva6@gmail.com>
2022-04-14 15:51:48 +0530

    Time: 44 ms (80.37%), Space: 34.9 MB (24.22%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -9,15 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Iterative Solution
+
+// class Solution {
+// public:
+//     TreeNode* searchBST(TreeNode* root, int val) {
+//         while(root!=nullptr && root->val!=val){
+//             if(root->val > val)
+//                 root=root->left;
+//             else
+//                 root=root->right;
+//         }
+//         return root;
+//     }
+// };
+
+//recursive solution
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        while(root!=nullptr && root->val!=val){
-            if(root->val > val)
-                root=root->left;
-            else
-                root=root->right;
-        }
+        if(root==nullptr ||root->val==val)
         return root;
+        if(root->val>val)
+            return searchBST(root->left,val);
+        else
+            return searchBST(root->right,val);
     }
 };
