c851bf0ed0763ef59cdeb273882669cf25730b86
Ayush srivastava <ayush.sriva6@gmail.com>
2022-04-14 18:22:10 +0530

    Time: 3 ms (88.25%), Space: 9.5 MB (79.27%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,18 +1,44 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n=matrix.size(),m=matrix[0].size();
+//         int i=0,j=m-1;
+//         while(i>=0 && i<n && j>=0 && j<m){
+//             if(matrix[i][j]==target)
+//                 return true;            
+//             else{
+//                 if(matrix[i][j]>target)
+//                     j--;                
+//                 else
+//                     i++;                
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size(),m=matrix[0].size();
-        int i=0,j=m-1;
-        while(i>=0 && i<n && j>=0 && j<m){
-            if(matrix[i][j]==target)
-                return true;            
-            else{
-                if(matrix[i][j]>target)
-                    j--;                
-                else
-                    i++;                
-            }
+        int n = matrix.size(), m = matrix[0].size(), i = 0, j = m - 1;
+			
+        while (i < n && j >= 0) {
+            int cur = matrix[i][j];
+            if (cur == target) return true;
+            if (target > cur) i++;
+            else j--;
         }
+        
         return false;
     }
 };
+
+
+
+
+
+
+
+
+
