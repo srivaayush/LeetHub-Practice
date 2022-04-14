bc901f0615b1cccd42cd9279ead14766e9c9bf88
Ayush srivastava <ayush.sriva6@gmail.com>
2022-04-14 18:23:58 +0530

    Time: 10 ms (18.62%), Space: 9.6 MB (53.11%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,41 +1,104 @@
+/*
+Absolute Brute Force
+TC ->> O(N*N)
+SC ->> O(1)
+*/
 // class Solution {
 // public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int n=matrix.size(),m=matrix[0].size();
-//         int i=0,j=m-1;
-//         while(i>=0 && i<n && j>=0 && j<m){
-//             if(matrix[i][j]==target)
-//                 return true;            
-//             else{
-//                 if(matrix[i][j]>target)
-//                     j--;                
-//                 else
-//                     i++;                
+//     bool searchMatrix(vector<vector<int>>& m, int target) {
+//         int r=m.size();
+//         int c=m[0].size();
+//         for(int i=0;i<r;i++){            
+//             for(int j=0;j<c;j++){
+//                 if(m[i][j]==target)
+//                 {
+//                     return true;
+//                 }                
 //             }
 //         }
-//         return false;
+//          return false;
 //     }
 // };
 
 
+/*
+Optimized Brute Force
+TC ->> O(N+N)
+SC ->> O(1)
+*/
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& m, int target) {
+//         int r=m.size();
+//         int c=m[0].size();
+//         for(int i=0;i<r;i++){
+//            if(m[i][c-1]<target)
+//                continue;
+//             for(int j=c-1;j>=0;j--){
+//                 if(m[i][j]==target)
+//                 {
+//                     return true;
+//                 }        
+//             }
+//             return false;
+//         }
+//          return false;
+//     }
+// };
+
+/*
+Better Approach
+TC ->> O(N*logN)
+SC ->> O(1)
+*/
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n=matrix.size();
+//         int m=matrix[0].size();      
+//         for(int i=0;i<n;i++)
+//         {
+//             if(matrix[i][m-1]<target)
+//                 continue;
+//             int l = 0, r = m;
+//             while(l<r){
+//                 int md=(l+r)/2;
+//                 if(matrix[i][md]==target)return true;
+//                 if(matrix[i][md]<target)l++;
+//                 else r--;
+//             }
+//         }        
+//         return false;
+//     }
+// };
+
+/*
+Optimized Approach
+TC ->> O(N)
+SC ->> O(1)
+*/
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size(), m = matrix[0].size(), i = 0, j = m - 1;
-			
-        while (i < n && j >= 0) {
-            int cur = matrix[i][j];
-            if (cur == target) return true;
-            if (target > cur) i++;
-            else j--;
-        }
-        
+        int n=matrix.size();
+        int m=matrix[0].size();  
+        int i=0,j=m-1;
+        while(i>=0 && i<n && j>=0 && j<m){
+            if(matrix[i][j]==target)
+                return true;            
+            else{
+                if(matrix[i][j]>target)
+                    j--;                
+                else
+                    i++;                
+            }
+        }      
         return false;
     }
 };
-
-
-
 
 
 
