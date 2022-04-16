d41100ad110319b0b7cbf35b1cae3e4367c4039e
Ayush srivastava <ayush.sriva6@gmail.com>
2022-04-17 02:40:33 +0530

    Time: 46 ms (66.97%), Space: 33.7 MB (9.65%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -9,16 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*RECURSIVE APPROACH
+TC - O(N)
+SC - O(N)
+*/
+// class Solution {
+// public:
+//     int sum=0;
+//     TreeNode* convertBST(TreeNode* root) {
+//         if(root==NULL) return NULL;
+//         convertBST(root->right);
+        
+//         sum+=root->val;
+//         root->val=sum;
+//         convertBST(root->left);
+//         return root;       
+//     }
+// };
+
 class Solution {
 public:
     int sum=0;
     TreeNode* convertBST(TreeNode* root) {
         if(root==NULL) return NULL;
-        convertBST(root->right);
-        
-        sum+=root->val;
-        root->val=sum;
-        convertBST(root->left);
-        return root;       
+        TreeNode *tempRoot=root;
+        stack<TreeNode*> st;
+        while(!st.empty() || tempRoot!=NULL){
+            while(tempRoot!=NULL){
+                st.push(tempRoot);
+                tempRoot=tempRoot->right;
+            }
+            tempRoot=st.top();
+            st.pop();
+            sum+=tempRoot->val;
+            tempRoot->val=sum;
+            tempRoot=tempRoot->left;
+        }
+        return root;
     }
 };
