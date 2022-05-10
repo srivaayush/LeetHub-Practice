5cd0b3a0f9f4bb01fb6d4e84a84e6d2e1e4f2a79
Ayush srivastava <ayush.sriva6@gmail.com>
2022-05-10 11:34:11 +0530

    Time: 8 ms (30.86%), Space: 11.2 MB (24.22%) - LeetHub

--- 969-pancake-sorting/969-pancake-sorting.cpp
+++ 969-pancake-sorting/969-pancake-sorting.cpp
@@ -4,14 +4,17 @@ public:
         int n=a.size();
         vector<int> ans;
         for(int i=n;i>0;i--){
+            bool f=true;
             int maxi=a[0];
             int maxindex=0;
             for(int j=0;j<i;j++){
+                if(j+1!=a[j])f=false;
                 if(maxi<a[j]){
                     maxi=a[j];
                     maxindex=j;
                 }
             }
+            if(f)return ans;
             // if(maxindex==i-1)return ans;
             ans.push_back(maxindex+1);
             ans.push_back(i);
