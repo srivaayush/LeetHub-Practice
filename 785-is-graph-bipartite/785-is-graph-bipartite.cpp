1dfc71be296c802f0bea26805616e5bb3c9c6108
Ayush srivastava <ayush.sriva6@gmail.com>
2022-05-26 17:57:04 +0530

    Time: 20 ms (96.81%), Space: 13.5 MB (79.81%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,35 +1,70 @@
+// class Solution
+// {
+//     public:
+//         bool isBipartite(vector<vector < int>> &graph)
+//         {
+//             int n = graph.size();
+//             vector<int> vis(n, 0);
+//                 queue<int> q;
+//             for (int i = 0; i < n; i++)
+//             {
+//                 if (vis[i]) continue;
+//                 vis[i]=1;
+//                 q.push(i);
+//                 while (!q.empty())
+//                 {
+//                     int qf = q.front();
+//                     for (auto &x: graph[qf])
+//                     {
+//                         if (vis[x] == 0)
+//                         {
+//                             vis[x] = -vis[qf];
+//                             q.push(x);
+//                         }
+//                         else if (vis[x] == vis[qf])
+//                         {
+//                             return false;
+//                         }
+//                     }
+//                     q.pop();
+//                 }
+//             }
+
+//             return true;
+//         }
+// };
+
 class Solution
 {
     public:
-        bool isBipartite(vector<vector < int>> &graph)
+        bool checkDFS(int node, vector<int> &col, vector<vector< int>> &graph)
         {
-            int n = graph.size();
-            vector<int> vis(n, 0);
-                queue<int> q;
-            for (int i = 0; i < n; i++)
+            if (col[node] == 0)
+                col[node] = 1;
+            for (auto &x: graph[node])
             {
-                if (vis[i]) continue;
-                vis[i]=1;
-                q.push(i);
-                while (!q.empty())
+                if (col[x] == 0)
                 {
-                    int qf = q.front();
-                    for (auto &x: graph[qf])
-                    {
-                        if (vis[x] == 0)
-                        {
-                            vis[x] = -vis[qf];
-                            q.push(x);
-                        }
-                        else if (vis[x] == vis[qf])
-                        {
-                            return false;
-                        }
-                    }
-                    q.pop();
+                    col[x] = -col[node];
+                    if (!checkDFS(x, col, graph))
+                        return false;
                 }
+                else if (col[x] == col[node]) return false;
             }
-
             return true;
         }
+    bool isBipartite(vector<vector < int>> &graph)
+    {
+        int n = graph.size();
+        vector<int> col(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            if (col[i] == 0)
+            {
+                if (!checkDFS(i, col, graph))
+                    return false;
+            }
+        }
+        return true;
+    }
 };
