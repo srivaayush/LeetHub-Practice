e569dca94c790e08dc344ef80fb1102813a1c46f
Ayush srivastava <ayush.sriva6@gmail.com>
2022-05-31 13:15:54 +0530

    Time: 3 ms (58.94%), Space: 6.9 MB (92.53%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,11 +3,14 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         
-        reverse(matrix.begin(),matrix.end());
+        // reverse(matrix.begin(),matrix.end());
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+            for(int j=i;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+        
+        for(auto &x:matrix)
+            reverse(x.begin(),x.end());
     }
 };
