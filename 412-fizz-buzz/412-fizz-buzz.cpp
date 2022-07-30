c1139bd57d1360167393e71af4a131dbb127a741
Ayush srivastava <ayush.sriva6@gmail.com>
2022-07-31 05:14:42 +0530

    Time: 6 ms (47.21%), Space: 8 MB (34.31%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    #define pb push_back
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0)                        
+                ans.pb("FizzBuzz");
+            else if(i%3==0)
+                ans.pb("Fizz");
+            else if(i%5==0)
+                ans.pb("Buzz");
+            else
+                ans.pb(to_string(i));
+        }
+        return ans;
+    }
+};
