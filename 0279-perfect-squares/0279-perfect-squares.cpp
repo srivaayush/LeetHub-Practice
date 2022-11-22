a151860d9c094dca2440a79f7e947e96d17696d6
Ayush srivastava <ayush.sriva6@gmail.com>
2022-11-22 11:39:10 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.44%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{  
+private:  
+    int is_square(int n)
+    {  
+        int sqrt_n = (int)(sqrt(n));  
+        return (sqrt_n*sqrt_n == n);  
+    }    
+public:
+    int numSquares(int n) 
+    {  
+        if(is_square(n)) 
+        {
+            return 1;  
+        }
+        while ((n & 3) == 0) // n%4 == 0  
+        {
+            n >>= 2;  
+        }
+        if ((n & 7) == 7) // n%8 == 7
+        {
+            return 4;
+        }
+        int sqrt_n = (int)(sqrt(n)); 
+        for(int i = 1; i <= sqrt_n; i++)
+        {  
+            if (is_square(n - i*i)) 
+            {
+                return 2;  
+            }
+        }  
+        
+        return 3;  
+    }  
+}; 
